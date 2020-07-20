39666634a61840e0593af0cd5948616925ad0fda
Mike Shenk <mlshenk415@gmail.com>
2020-07-19 21:54:04 -0700

    code mostly written, buttons complete

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES 
+("Royal Red Robin", true), 
+("Double Quarter Pounder", true), 
+("Whataburger Patty Melt", false), 
+("The Grilled Cheesus", false)
